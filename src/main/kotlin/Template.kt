8f158043c7414fbe93320e7d36e67b8259f08d9e
Joe Shea <jsshea82@gmail.com>
2022-12-06 21:08:38 +0100

    Create template for daily boilerplate

---
+++ src/main/kotlin/Template.kt
@@ -0,0 +1,13 @@
+fun main() {
+    val input = readInput("DayXX")
+    part1(input)
+    part2(input)
+}
+
+private fun part1(input: List<String>) {
+
+}
+
+private fun part2(input: List<String>) {
+
+}
